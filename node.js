481b1517edb1341d36fd76ccf436f18a1ecf41e6
Jimmy Zhang <jimmy@observablehq.com>
2022-12-22 12:11:58 -0500

    ideation phase done. starting to fuck shit up. getting ready for the real blog post;;

--- node.js
+++ node.js
@@ -6,6 +6,7 @@ class Node {
     static VISITED = 2;
     static LEFT = "L";
     static RIGHT = "R";
+    static DELETE = "delete";
     constructor(nodeId) {
         this.nodeId = nodeId;
         this.lineIdx = 0; // Each frame already starts at line #2.
@@ -100,10 +101,11 @@ class Node {
                 n.addClass("active");
             } else {
                 node.addClass("active");
+                node.removeClass("visited");
                 // THIS IS A HACK: assumes that the text label node is always the first node in the HTML.
                 this.getGraphNode().find("text").first().removeClass("visited");
             }
-        } else if (state === Node) {
+        } else if (state === Node.INACTIVE) {
             node.removeClass("active");
         } else if (state === Node.VISITED) {
             if (this.isNull()) {
@@ -127,10 +129,6 @@ class Node {
                 node.removeClass("active");
             }
         }
-    }
-
-    getRootCell() {
-
     }
 
     connect(activate) {
@@ -182,13 +180,13 @@ class Node {
     // Different than deactivate, is called when another node is placed on top of this frame.
     destroy() {
         const frame = this.getFrame();
+        frame.addClass(Node.DELETE);
         $(`#connector-${this.nodeId}`).remove();
         this.markGraphNode(Node.VISITED);
-        frame.remove();
     }
 
-    // restore the infromation about this node
-    restore() {
+    // reset all the state about this node.
+    resetState() {
         const frame = this.getFrame();
         $(`#connector-${this.nodeId}`).remove();
         frame.remove();
@@ -197,32 +195,32 @@ class Node {
 
     // Insert code frame for this node.
     createFrame(counter) {
-        const elem = $(`<div id="frame-${this.nodeId}-container" class="flex active frame line-numbers"><pre data-line=${this.getCurrentLineNumber()} id="frame-${this.nodeId}"><code class="language-python">def dfs(root):
-  if not root:
+        const elem = $(`<div id="frame-${this.nodeId}-container" class="flex active frame line-numbers"><pre data-line=${this.getCurrentLineNumber()} id="frame-${this.nodeId}"><code class="language-python">def dfs(node):
+  if not node:
     return
 
-  print(root.value)
-  dfs(root.left)
-  dfs(root.right)
+  print(node.value)
+  dfs(node.left)
+  dfs(node.right)
 </code>
 </pre>
 <div class="variables">
     <table class="variable-grid">
         <tr class="first-row">
-            <td class="first-cell">root</td>
+            <td class="first-cell">node</td>
             <td class="root-cell"></td>
         </tr>
-        
-        
     </table>
 </div>
 </div>`);
 
         elem.css({
-            left: `${(counter) * 210 + 30}px`,
+            left: `${(counter) * 150 + 30}px`,
             top: `${(counter) * 80 + 30}px`
         });
         this.advance(); // lineIdx always points to the NEXT line of code to be executed.
+
+
         return elem;
     }
 }
